e67427ad6708dca500ea74138d14ee38ab44ed42
woxxo <lesf1@hotmail.com>
2024-12-10 22:47:55 +0300

    5

--- test.jsx
+++ test.jsx
@@ -1,12 +1,12 @@
-import { renderToHtml } from 'jsxlib';
+import { renderToHtml } from 'jsxlib/jsx';
 import { name } from 'jsxlib';
 
 const App = ({ name, children }) => {
-    return <div>{name} {children}</div>;
+    return <div>I have name "{name}" and children {children}</div>;
 };
 
 console.log(name);
-console.log(<h1>aaaaa</h1>);
+console.log(<h1 info="Header" date={new Date(Date.now()).getFullYear()}>aaaaa</h1>);
 console.log(<><p>11</p><p>22</p></>);
-console.log(renderToHtml(<div>123</div>, { pretty: true }));
+console.log(renderToHtml(<div date={new Date(Date.now()).getFullYear()}>123</div>, { pretty: true }));
 console.log(renderToHtml(<App name="Name1"><h6>header6</h6><p>ppppp</p></App>, { pretty: true }));
